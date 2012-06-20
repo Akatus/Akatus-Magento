08d572bac3b3718d566ba39d18ea6b734168d6cb
Mauricio SAlles <mauricio@mauricio-Vostro-3500.(none)>
2012-06-20 20:11:08 -0300

    Commit  Inicial

---
+++ ModuloMagentoFinal/ModuloMagentoFinal/Akatus/app/code/community/Akatus/Akatus/Model/Resource/Mysql4/Setup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Magento Akatus Payment Modulo
+ *
+ * @category   Mage
+ * @package    Akatus_Akatus
+ * @copyright  Author 
+ */
+
+
+
+class Akatus_Akatus_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup 
+{
+	
+	
+	
+	
+}
+
