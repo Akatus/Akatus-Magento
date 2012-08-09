51af8e598045e687d34b83ed126bbbad0724fec5
mrsalles <mauriciorsalles@gmail.com>
2012-08-09 17:08:52 -0300

    Reestruturação de diretṕorio

---
+++ app/code/community/Akatus/Akatus/Block/Info/Pay.php
@@ -0,0 +1,125 @@
+﻿<?php
+class Akatus_Akatus_Block_Info_Pay extends Mage_Payment_Block_Info
+{
+	protected function _prepareSpecificInformation($transport = null)
+	{
+		if (null !== $this->_akatusSpecificInformation) {
+			return $this->_akatusSpecificInformation;
+		}
+		$info = $this->getInfo();
+		$transport = new Varien_Object();
+		$transport = parent::_prepareSpecificInformation($transport);
+		/*
+		switch($info->getCheckFormapagamento()){
+			case "boleto":
+				$formaPagamento = "Boleto Bancário";
+				break;
+			case "cartaodecredito":
+				$formaPagamento = "Cartão de Crédito";
+				break;
+			case "tef":
+				$formaPagamento = "Cartão de Débito - TEF";
+				break;
+		}
+		/*$array = array(
+			Mage::helper('payment')->__('Forma de Pagamento') => $formaPagamento
+		);*/
+
+		#var_dump($info);
+	
+	if($info->getCheckFormapagamento() == 'boleto'){
+			$array = array(
+				Mage::helper('payment')->__('Forma de Pagamento') => $info->getCheckFormapagamento(),
+				Mage::helper('payment')->__('Forma de Pagamento') => utf8_encode("Boleto Bancário"),
+				Mage::helper('payment')->__('Segunda Via') => $info->getCheckBoletourl()
+			);
+
+			echo ("<table>
+                                    <tbody>
+                                        <tr>
+                                            <th>
+                                            <strong>Forma de Pagamento:</strong>
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            <td>Boleto Bancário</td>
+                                        </tr>
+                                        <tr>
+                                            <th>
+                                            <strong>Segunda Via</strong>
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            <td><a href = '{$info->getCheckBoletourl()}'>Imprimir</a></td>
+                                        </tr>
+                                    </tbody>
+                                </table>");
+		#	echo utf8_encode("<a href = '{$info->getCheckBoletourl()}'>Imprimir 2 via</a>");
+
+		}elseif($info->getCheckFormapagamento() == 'cartaodecredito'){
+			$checkBandCC = $info->getCheckCartaobandeira();
+			if($checkBandCC == "cartao_amex"){
+
+				$numeroCartao = $info->getCheckNumerocartao();
+				$last5 = substr($numeroCartao,(strlen($numeroCartao)-5),strlen($numeroCartao));
+
+				$numCart = "XXXX.XXXXXX." . $last5;
+
+			}else{
+
+				$numeroCartao = $info->getCheckNumerocartao();
+				$last4 = substr($numeroCartao,(strlen($numeroCartao)-4),strlen($numeroCartao));
+
+				$numCart = "XXXX.XXXX.XXXX." . $last4;
+
+			}
+			
+			$cartaoLabel = str_replace("cc_", "", $info->getCheckCartaobandeira());
+			switch($cartaoLabel){
+				case "cartao_amex":
+					$cartao = "Cartão American Express";
+					break;
+				case "cartao_elo":
+					$cartao = "Cartão Elo";
+					break;
+				case "cartao_master":
+					$cartao = "Cartão Master";
+					break;
+				case "cartao_diners":
+					$cartao = "Cartão Diners";
+					break;
+				case "cartao_visa":
+					$cartao = "Cartão Visa";
+					break;					
+			}
+			$array = array(
+				(Mage::helper('payment')->__('Bandeira do Cartão')) => ($cartao),
+				Mage::helper('payment')->__('Nome') => $info->getCheckNome(),
+				Mage::helper('payment')->__('Cpf') => $info->getCheckCpf(),
+				(Mage::helper('payment')->__('Numero do Cartão')) => $numCart,
+				#Mage::helper('payment')->__('Expiracao Mês') => $info->getCheckExpiracaomes(),
+				#Mage::helper('payment')->__('Check Expiracaoano') => $info->getCheckExpiracaoano(),
+
+				#Mage::helper('payment')->__('Check Codseguranca') => $info->getCheckCodseguranca()
+			);
+		}else{
+
+			$array = array(
+
+				Mage::helper('payment')->__('Bandeira') => $info->getCheckTefbandeira()
+
+			);
+		}
+
+                      /*Mage::helper('payment')->__('Check Cartaobandeira') => $info->getCheckCartaobandeira(),
+			Mage::helper('payment')->__('Check Nome') => $info->getCheckNome(),
+			Mage::helper('payment')->__('Check Cpf') => $info->getCheckCpf(),
+			Mage::helper('payment')->__('Check Numerocartao') => $info->getCheckNumerocartao(),
+			Mage::helper('payment')->__('Check Expiracaomes') => $info->getCheckExpiracaomes(),
+			Mage::helper('payment')->__('Check Expiracaoano') => $info->getCheckExpiracaoano(),
+			Mage::helper('payment')->__('Check Codseguranca') => $info->getCheckCodseguranca(),
+			Mage::helper('payment')->__('Check Tefbandeira') => $info->getCheckTefbandeira(),*/
+		$transport->addData($array);
+		return $transport;
+	}
+}
---
+++ app/code/community/Akatus/Akatus/sql/akatus_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * @category   Mage
+ * @package    Akatus
+ * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+$installer = $this;
+/* @var $installer Akatus_Model_Mysql4_Setup */
+
+//print_r($installer->getTable('sales/order'));
+
+$installer->startSetup();
+
+
+$prefix = Mage::getConfig()->getTablePrefix();
+
+
+$installer->run("CREATE TABLE IF NOT EXISTS `akatus_transacoes` ( `id` INT NULL AUTO_INCREMENT ,
+                                                                  `idpedido` INT NOT NULL ,
+                                                                  `codtransacao` VARCHAR( 255 ) NOT NULL ,
+                                                                  PRIMARY KEY ( `id` ) 
+                                                                  ) ENGINE = InnoDB");
+
+
+$installer->run($query);
+
+$query = "ALTER TABLE `".$prefix."sales_flat_order_payment`
+				 ADD `check_no` VARCHAR(20) NOT NULL,
+				 ADD `check_date` VARCHAR(20) NOT NULL,
+				 ADD `check_cartaobandeira` VARCHAR(20) NOT NULL,
+				 ADD `check_nome` VARCHAR(200) NOT NULL,
+				 ADD `check_cpf` VARCHAR(30) NOT NULL,
+			 	 ADD `check_numerocartao` VARCHAR(20) NOT NULL,
+				 ADD `check_expiracaomes` VARCHAR(4) NOT NULL,
+				 ADD `check_expiracaoano` VARCHAR(4) NOT NULL,
+				 ADD `check_codseguranca` VARCHAR(5) NOT NULL,
+				 ADD `check_parcelamento` VARCHAR(10) NOT NULL,
+				 ADD `check_tefbandeira` VARCHAR(40) NOT NULL,
+				 ADD `check_formapagamento` VARCHAR(40) NOT NULL,
+				 ADD `check_boletourl` VARCHAR(200) NOT NULL";
+
+
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_no', 'varchar(20) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_date', 'varchar(20) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_cartaobandeira', 'varchar(20) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_nome', 'varchar(200) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_cpf', 'varchar(30) NOT NULL');$
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_numerocartao', 'varchar(20) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_expiracaomes', 'varchar(4) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_expiracaoano', 'varchar(4) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_codseguranca', 'varchar(5) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_parcelamento', 'varchar(10) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_tefbandeira', 'varchar(40) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_formapagamento', 'varchar(40) NOT NULL');
+$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'check_boletourl', 'varchar(200) NOT NULL');
+
+
+
+$installer->run($query);
+
+
+$installer->endSetup();
