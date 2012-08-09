6ad60af38d1928679e872919e76bd20832eb9d47
mrsalles <mauriciorsalles@gmail.com>
2012-08-09 17:07:59 -0300

    Adição de suporte a cupom e também parcelamento dinâmico

--- Akatus/app/code/community/Akatus/Akatus/Block/Form/Pay.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Akatus_Akatus_Block_Form_Pay extends Mage_Payment_Block_Form
-{
-	protected function _construct()
-	{
-		parent::_construct();
-		$this->setTemplate('akatus/form/pay.phtml');
-	}
-}
