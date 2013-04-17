eb669be1498867564c7d4887393c56ee16c9d5b7
Diego Souza <diego.souza@akatus.com>
2013-04-17 14:42:45 -0300

    Arquivo necessário p/ gerenciamento do número máximo de parcelas (não foi no commit anterior)

---
+++ app/code/community/Akatus/Akatus/Adminhtml/Model/NumeroMaximoParcelas.php
@@ -0,0 +1,22 @@
+<?php
+
+class Akatus_Akatus_Adminhtml_Model_NumeroMaximoParcelas
+{
+  public function toOptionArray()
+  {
+    return array(
+      array('value' => 1, 'label' => 1),
+      array('value' => 2, 'label' => 2),
+      array('value' => 3, 'label' => 3),
+      array('value' => 4, 'label' => 4),
+      array('value' => 5, 'label' => 5),
+      array('value' => 6, 'label' => 6),
+      array('value' => 7, 'label' => 7),
+      array('value' => 8, 'label' => 8),
+      array('value' => 9, 'label' => 9),
+      array('value' => 10, 'label' => 10),
+      array('value' => 11, 'label' => 11),
+      array('value' => 12, 'label' => 12),
+    );
+  }
+}
